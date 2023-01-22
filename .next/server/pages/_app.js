0f7c99f9183cf09b435beaf0adf61a3dd44730bd
ritughiya <ritughiya@gmail.com>
2023-01-22 18:10:01 -0500

    first

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,57 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/_app";
+exports.ids = ["pages/_app"];
+exports.modules = {
+
+/***/ "./pages/_app.js":
+/*!***********************!*\
+  !*** ./pages/_app.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ MyApp)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles/globals.css */ \"./styles/globals.css\");\n/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_globals_css__WEBPACK_IMPORTED_MODULE_1__);\n\n\n// This default export is required in a new `pages/_app.js` file.\nfunction MyApp({ Component , pageProps  }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n            ...pageProps\n        }, void 0, false, {\n            fileName: \"/Users/ritu/Dropbox/local/collagepoetics/pages/_app.js\",\n            lineNumber: 11,\n            columnNumber: 7\n        }, this)\n    }, void 0, false);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9fYXBwLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFBOEI7QUFHOUIsaUVBQWlFO0FBQ2xELFNBQVNBLE1BQU0sRUFBRUMsVUFBUyxFQUFFQyxVQUFTLEVBQUUsRUFBRTtJQUN0RCxxQkFDRTtrQkFJRSw0RUFBQ0Q7WUFBVyxHQUFHQyxTQUFTOzs7Ozs7O0FBSzlCLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9wYWdlcy9fYXBwLmpzP2UwYWQiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICcuLi9zdHlsZXMvZ2xvYmFscy5jc3MnXG5cblxuLy8gVGhpcyBkZWZhdWx0IGV4cG9ydCBpcyByZXF1aXJlZCBpbiBhIG5ldyBgcGFnZXMvX2FwcC5qc2AgZmlsZS5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIE15QXBwKHsgQ29tcG9uZW50LCBwYWdlUHJvcHMgfSkge1xuICByZXR1cm4gKFxuICAgIDw+XG5cblxuXG4gICAgICA8Q29tcG9uZW50IHsuLi5wYWdlUHJvcHN9IC8+XG5cbiAgICA8Lz5cbiAgXG4gIClcbn0iXSwibmFtZXMiOlsiTXlBcHAiLCJDb21wb25lbnQiLCJwYWdlUHJvcHMiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/_app.js\n");
+
+/***/ }),
+
+/***/ "./styles/globals.css":
+/*!****************************!*\
+  !*** ./styles/globals.css ***!
+  \****************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/_app.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/index.js
@@ -0,0 +1,140 @@
+import 'animate.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import Head from 'next/head';
+
+function HomePage() {
+
+
+  // Random Region
+
+//   var randomImg = [
+//     '<img src="/pic.png"></img>',
+// '<img src="/pic1.jpg"></img>',
+// '<img src="/pic2.png"></img>',
+// '<img src="/pic3.png"></img>',
+// '<img src="/pic4.png"></img>',
+// '<img src="/pic5.png"></img>',
+
+//   ]
+//   const out = []
+
+//   for (let i = 0; i < randomImg.length; i++) {
+//     out.push(randomImg.splice(Math.floor(Math.random() * randomImg.length), 1))
+//   }
+
+  return (
+    <>
+    <Head>
+    <title>collage poetics 2023</title>
+    </Head>
+  <div className="text">
+  <div className="cloudheader">
+    {/* <div className="cloudh1">
+        <div className="">
+            <a href="#">
+            Resources
+            </a>
+        </div>
+        <div className="">
+        <a href="#">
+            Inspiration
+            </a>
+        </div>
+    </div> */}
+<img src="/cloud.png" layout="fill"></img>
+
+</div>
+    <div className="header">
+    <img src="/CollagePoetics.png" layout="fill"></img>
+    </div>
+
+    <div className="container">
+
+        <div className="group animate__animated animate__bounceInDown">
+            Syllabus
+            <div className="card default">
+            <a target="_blank" href="https://docs.google.com/document/d/1FAHL7zy8U_gcXnkgR9roEQUo_wpfH9uXnH4tzgRgv_M/edit?usp=sharing">
+            <div className="link"></div>
+</a>
+            </div>
+        </div>
+        <div className="row">
+
+        <div className="group">
+            Lecture: On Collage
+            <div className="card transparent">
+            <a target="_blank" href="/Collage-Poetics-2023-Day-1.pdf">
+            <div className="link">
+
+            <div className="nodal">
+                    <div className="dot"></div>
+                </div>
+                <div className="point">
+
+                </div>
+
+            </div>
+</a>
+
+
+            </div>
+        </div>
+        <div className="group">
+        Orientations of a Queer Phenomenology by Sara Ahmed
+            <div className="cardcontainer">
+            <div className="card sara">
+            <a href="/Excerpt-Orientations+Toward+a+Queer+Phenomenology.pdf" target="_blank">
+            <img src="sweatshirt.jpg"></img>
+            </a>
+
+            </div>            
+
+            </div>
+        </div>
+        </div>
+        <div className="row">
+
+<div className="group">
+ Journal Prompts
+    <div className="cardcontainer">
+    <div className="card amiri">
+        <a href="https://docs.google.com/document/d/17thFROeYoqlPC47DXvapOj9uYkCKLvwbkHZBQEZFyVk/edit?usp=sharing" target="_blank">
+    <img src="fashion.jpg"></img>
+    </a>
+    </div>            
+    </div>
+</div>
+<div className="group">
+        Surveys
+            <div className="cardcontainer">
+            <div className="card orange">
+            <a target="_blank" href="https://forms.gle/xZvHGd5wWM2w4nVy8">
+            <div className="link">
+                </div>
+                </a>
+            </div>            
+            <div className="card purple">
+            <a target="_blank" href="https://forms.gle/9rCxvDbrYqHBepnL8">
+            <div className="link">
+                </div>
+                </a>
+            </div>
+            </div>
+        </div>
+</div>
+    </div>
+    <footer>
+        <div className="websitewiki">
+        This website is made to house the syllabus and the archive of Yale MFA course Collage Poetics’ spring 2023 semester. It launched the first day of the course 1/23/23 and will reveal itself fully through 4/24/23 the final day of the course.
+        </div>
+        <div>
+
+        </div>
+    </footer>
+  </div>
+  </>
+  )
+}
+
+export default HomePage
