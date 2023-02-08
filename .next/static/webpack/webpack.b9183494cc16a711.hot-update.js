57efb67235eb070552c15a9afdbe673773a7d37e
ritughiya <ritughiya@gmail.com>
2023-02-07 19:52:23 -0500

    change font

--- .next/static/webpack/webpack.b9183494cc16a711.hot-update.js
+++ .next/static/webpack/webpack.b9183494cc16a711.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "efa90a41369ca517"; }
+/******/ 	__webpack_require__.h = function() { return "baddfbb2a05ae526"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
